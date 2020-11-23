2d8179cc72d97837cbabf75a165d94027c84e031
Amanda Diana <amandadianacmd@gmail.com>
2020-11-23 15:57:34 +0100

    posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { Post } from "./post.model";
 
 @Component({
   selector: "app-root",
@@ -8,7 +9,8 @@ import { map } from "rxjs/operators";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+  isFetching: boolean = false;
 
   constructor(private http: HttpClient) {}
 
@@ -16,10 +18,13 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post("https://maxi-ang.firebaseio.com/posts.json", postData)
+      .post<{ name: string }>(
+        "https://maxi-ang.firebaseio.com/posts.json",
+        postData
+      )
       .subscribe((responseData) => {
         console.log(responseData);
       });
@@ -35,11 +40,14 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
-      .get("https://maxi-ang.firebaseio.com/posts.json")
+      .get<{ [key: string]: Post }>(
+        "https://maxi-ang.firebaseio.com/posts.json"
+      )
       .pipe(
-        map((responseData) => {
-          const postsArray = [];
+        map((responseData: { [key: string]: Post }) => {
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
               postsArray.push({ ...responseData[key], id: key });
@@ -49,7 +57,8 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
-        console.log(posts);
+        this.isFetching = false;
+        this.loadedPosts = posts;
       });
   }
 }
